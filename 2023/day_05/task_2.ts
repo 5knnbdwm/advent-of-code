a8d75784ee0f63e8f32e967a607f3a2327b8ca3d
5knnbdwm <samuel@soeller.dev>
2023-12-06 09:02:28 +0100

    2023 day 05 2 - fixed

--- 2023/day_05/task_2.ts
+++ 2023/day_05/task_2.ts
@@ -1,78 +1,10 @@
-const {default: data} = require('./input.txt');
-// const {default: data} = require('./file_input.txt');
+const {default: data} = require('./file_input.txt');
 // const {default: data} = require('./file_test.txt');
-
-// const transformMap = (value: number, group: string) => {
-//   const lines = group.split('\n');
-//   lines.shift();
-//
-//   let a: Map<string, string> = new Map()
-//
-//   let result = value
-//
-//   for (const line of lines) {
-//     const [destStart, sourceStart, length] = line.split(' ').map(Number);
-//
-//     if (value >= sourceStart && value <= sourceStart + length) {
-//       result = destStart + (value - sourceStart)
-//       break
-//     }
-//   }
-//
-//   return result
-// }
-
-// const transform = (value: number, groups: string[]) => {
-//   let val1 = transformMap(value, groups[1])
-//   let val2 = transformMap(val1, groups[2])
-//   let val5 = transformMap(val2, groups[5])
-//   let val3 = transformMap(val5, groups[3])
-//   let val4 = transformMap(val3, groups[4])
-//   let val6 = transformMap(val4, groups[6])
-//   let val7 = transformMap(val6, groups[7].replace(/\n$/, ''))
-//
-//   return val7
-// }
-
 
 export default function run() {
   const input = data.split('\n\n');
-  const seedRanges = input[0].split(': ')[1].replace(/(\d+\s\d+)\s/gm, '$1\n').split('\n').map(x => x.split(' ').map(Number))
-  const transformations = input.slice(1).map(x => x.split('\n').slice(1).map(y => y.split(' ').map(Number)))
-
-  let result = -1
-
-  // for (const seed of seeds) {
-  //   for (const transformation of transformations) {
-  //     let n = 0
-  //     for (const transformationElement of transformation) {
-  //       console.log(transformationElement)
-  //       const destinationStart = transformationElement[0]
-  //       const sourceStart = transformationElement[1]
-  //       const length = transformationElement[2]
-  //
-  //       if (seed >= sourceStart && seed <= sourceStart + length)
-  //         n = destinationStart + (seed - sourceStart)
-  //     }
-  //     console.log(n)
-  //   }
-  //   break
-  // }
-
-  // const transform = (seed: number) => {
-  //   for (const transformation of transformations) {
-  //     for (const [destinationStart, sourceStart, length] of transformation) {
-  //       if (seed >= sourceStart && seed <= sourceStart + length) {
-  //         seed = destinationStart + (seed - sourceStart)
-  //         break
-  //       }
-  //     }
-  //   }
-  //   return seed
-  // }
-  //
-  //
-  // return seeds.map(s => transform(s))
+  const seedRanges: number[][] = input[0].split(': ')[1].replace(/(\d+\s\d+)\s/gm, '$1\n').split('\n').map((x: string) => x.split(' ').map(Number))
+  const transformations: number[][][] = input.slice(1).map((x: string) => x.split('\n').slice(1).map((y: string) => y.split(' ').map(Number)))
 
   const isSeed = (seed: number): boolean => seedRanges
     .some(([seedStart, length]) => seedStart <= seed && seedStart + length >= seed);
@@ -95,7 +27,6 @@ export default function run() {
 
     if (isSeed(seed)) {
       return i
-      break;
     }
   }
 }
