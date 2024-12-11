84f150e7302b943d8bff22d350656686da13ba6c
Samuel Soeller <samuel@soeller.dev>
2024-12-11 12:29:30 +0100

    2024 day 10 formating

--- 2024/day_10/task_1.ts
+++ 2024/day_10/task_1.ts
@@ -1,4 +1,4 @@
-const {default: data}: {default: string} = require('./file_input.txt');
+const { default: data }: { default: string } = require("./file_input.txt");
 // const {default: data}: {default: string} = require('./file_test.txt');
 
 // const data = `10..9..
@@ -19,14 +19,14 @@ function join(p: Point) {
 }
 
 const adjacents = [
-  {x: -1, y: 0},
-  {x: 1, y: 0},
-  {x: 0, y: -1},
-  {x: 0, y: 1},
+  { x: -1, y: 0 },
+  { x: 1, y: 0 },
+  { x: 0, y: -1 },
+  { x: 0, y: 1 },
 ];
 
 export default function run() {
-  const grid = data.split('\n').map(line => line.split('').map(Number));
+  const grid = data.split("\n").map((line) => line.split("").map(Number));
   let score = 0;
 
   const trailHeads = {} as { [key: string]: string[] };
@@ -35,51 +35,59 @@ export default function run() {
     for (let j = 0; j < grid[i].length; j++) {
       // console.log(join({x: i, y: j}), grid[j][i]);
       if (grid[i][j] === 0) {
-        let queue = [{x: i, y: j}] as Point[];
-        let visited = [] as Point[]; 
+        let queue = [{ x: i, y: j }] as Point[];
+        let visited = [] as Point[];
 
         while (queue.length > 0) {
           const current = queue.shift() as Point;
-          // console.log(join(current), grid[current.x][current.y], 'bfs', 'v:', JSON.stringify(visited.map(join)), 'q:', JSON.stringify(queue.map(join))); 
+          // console.log(join(current), grid[current.x][current.y], 'bfs', 'v:', JSON.stringify(visited.map(join)), 'q:', JSON.stringify(queue.map(join)));
           visited.push(current);
-
 
           if (grid[current.x][current.y] === 9) {
             // console.log(join(current), grid[current.x][current.y]);
-            if (!(join({x: i, y: j}) in trailHeads)) trailHeads[join({x: i, y: j})] = [];
-            const currentTrail = trailHeads[join({x: i, y: j})];
+            if (!(join({ x: i, y: j }) in trailHeads))
+              trailHeads[join({ x: i, y: j })] = [];
+            const currentTrail = trailHeads[join({ x: i, y: j })];
 
             if (!currentTrail.includes(join(current))) {
               currentTrail.push(join(current));
             }
             continue;
           }
-          
+
           for (const adjacent of adjacents) {
-            const next = {x: current.x + adjacent.x, y: current.y + adjacent.y};
+            const next = {
+              x: current.x + adjacent.x,
+              y: current.y + adjacent.y,
+            };
             const currentHeight = grid[current.x][current.y];
 
-            if (next.x < 0 || next.x >= grid.length || next.y < 0 || next.y >= grid[0].length) continue;
-            
+            if (
+              next.x < 0 ||
+              next.x >= grid.length ||
+              next.y < 0 ||
+              next.y >= grid[0].length
+            )
+              continue;
+
             const nextHeight = grid?.[next.x]?.[next.y];
             // console.log(nextHeight, currentHeight);
             if (nextHeight !== undefined && nextHeight - 1 === currentHeight) {
               queue.push(next);
             }
           }
-          // console.log(join(current), grid[current.x][current.y], 'bfs', 'v:', JSON.stringify(visited.map(join)), 'q:', JSON.stringify(queue.map(join))); 
+          // console.log(join(current), grid[current.x][current.y], 'bfs', 'v:', JSON.stringify(visited.map(join)), 'q:', JSON.stringify(queue.map(join)));
         }
       }
     }
   }
 
-  
   // console.log('trailHeads', trailHeads);
-  Object.keys(trailHeads).forEach(key => {
+  Object.keys(trailHeads).forEach((key) => {
     score += trailHeads[key].length;
   });
 
-  return score
+  return score;
 }
 
-console.log('result:', run())
+console.log("result:", run());
